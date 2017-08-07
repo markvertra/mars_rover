6c27ed843ce0dd8bd0aeba50f73e3700d7555784
Mark Allen <mark@traversify.com>
2017-08-07 21:31:55 +0200

    Rover grid and backwards command

--- rover.js
+++ rover.js
@@ -1,7 +1,18 @@
 var myRover = {
-  position: [0,0], 
+  position: [0,0],
   direction: 'N'
 };
+
+roverGrid = [[0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
+             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9]]
 
 function goForward(rover) {
   switch(rover.direction) {
@@ -22,4 +33,35 @@ function goForward(rover) {
   console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
 }
 
-goForward(myRover);
+function goBackward(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.position[0]--
+      break;
+    case 'E':
+      rover.position[1]--
+      break;
+    case 'S':
+      rover.position[0]++
+      break;
+    case 'W':
+      rover.position[1]++
+      break;
+    };
+  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  };
+
+  function movement(rover, commands) {
+    for (i = 0; i < commands.length; i++) {
+      switch(commands[i]) {
+        case 'f':
+        goForward(rover);
+        break;
+        case 'b':
+        goBackward(rover);
+        break;
+      }
+    }
+  }
+
+movement(myRover, ['f', 'f', 'b'])
