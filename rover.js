8a0fba1ad265756e8d67223fa7d5514b4580d12b
Mark Allen <mark@traversify.com>
2017-08-07 22:04:55 +0200

    Rover right and left

--- rover.js
+++ rover.js
@@ -3,65 +3,105 @@ var myRover = {
   direction: 'N'
 };
 
-roverGrid = [[0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9],
-             [0, 1, 2, 3, 4, 5, 6, 7, 8 ,9]]
+roverGrid = [['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x'],
+             ['x', 'x', 'x', 'x', 'x', 'x', 'x', 'x', 'x' ,'x']];
 
 function goForward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]++
+      rover.position[0]++;
       break;
     case 'E':
-      rover.position[1]++
+      rover.position[1]++;
       break;
     case 'S':
-      rover.position[0]--
+      rover.position[0]--;
       break;
     case 'W':
-      rover.position[1]--
+      rover.position[1]--;
       break;
-  };
-
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  }
 }
 
 function goBackward(rover) {
   switch(rover.direction) {
     case 'N':
-      rover.position[0]--
+      rover.position[0]--;
       break;
     case 'E':
-      rover.position[1]--
+      rover.position[1]--;
       break;
     case 'S':
-      rover.position[0]++
+      rover.position[0]++;
       break;
     case 'W':
-      rover.position[1]++
+      rover.position[1]++;
       break;
-    };
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
-  };
-
-  function movement(rover, commands) {
-    for (i = 0; i < commands.length; i++) {
-      switch(commands[i]) {
-        case 'f':
-        goForward(rover);
-        break;
-        case 'b':
-        goBackward(rover);
-        break;
-      }
     }
   }
 
-movement(myRover, ['f', 'f', 'b'])
+function turnRight(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'E';
+      break;
+    case 'E':
+      rover.direction = 'S';
+      break;
+    case 'S':
+      rover.direction = 'W';
+      break;
+    case 'W':
+      rover.direction = 'N';
+      break;
+    }
+  }
+
+function turnLeft(rover) {
+  switch(rover.direction) {
+    case 'N':
+      rover.direction = 'W';
+      break;
+    case 'E':
+      rover.direction = 'N';
+      break;
+    case 'S':
+      rover.direction = 'E';
+      break;
+    case 'W':
+      rover.direction = 'S';
+      break;
+      }
+    }
+
+function movement(rover, commands) {
+  for (i = 0; i < commands.length; i++) {
+    switch(commands[i]) {
+      case 'f':
+        goForward(rover);
+        break;
+      case 'b':
+        goBackward(rover);
+        break;
+      case 'l':
+        turnLeft(rover);
+        break;
+      case 'r':
+        turnRight(rover);
+        break;
+        }
+      console.log("Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]");
+      console.log("River Direction: " + rover.direction);
+      }
+
+    }
+
+movement(myRover, ['f', 'f', 'b', 'l', 'f', 'r', 'b']);
